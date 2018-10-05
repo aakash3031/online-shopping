48508421319912622f19ff6277873ece3d969502
aakash3031 <aakashkumarmiglani@gmail.com>
2018-10-05 23:13:14 +0530

    Static Resources Loaded

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageHandler.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageHandler.java
@@ -1,7 +1,9 @@
 package net.kzn.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,5 +16,6 @@ public class PageHandler {
 		mv.addObject("greeting","Welcome to Spring Web MVC");
 		return mv;
 	}
+	
 }
 
